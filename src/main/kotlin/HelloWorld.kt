e775d38e60b8c50c918265f211fb53a6e2ffe202
Tony McCrae <tony@eelpieconsulting.co.uk>
2018-11-09 11:48:40 +0000

    Early return is still allowed =(

--- src/main/kotlin/HelloWorld.kt
+++ src/main/kotlin/HelloWorld.kt
@@ -1,10 +1,19 @@
-fun meh() = "meh"
+val a = 2
 
-val a = 1
+fun meh(): String {
+    if (a > 1) {
+        return "ok"
+    } else {
+        return "meh"
+    }
+}
 
 fun main(args: Array<String>) {
     var mutable = a
     mutable = mutable + 1
+    mutable += 1
+
+    val teamplate = "Hello $a"
 
     println("Hello: " + meh() + mutable)
 }
