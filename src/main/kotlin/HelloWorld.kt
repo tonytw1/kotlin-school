e0a3003d19d19fa95b32637dd7f37501f8e8bc3f
Tony McCrae <tony@eelpieconsulting.co.uk>
2018-11-09 11:56:01 +0000

    Map works as expected.

--- src/main/kotlin/HelloWorld.kt
+++ src/main/kotlin/HelloWorld.kt
@@ -7,5 +7,10 @@ fun meh(a: Int) = if (a > 1) {
 fun main(args: Array<String>) {
     val things = listOf("a", "b", "c")
     val a = things.size
+
+    things.map { t ->
+        println(t)
+    }
+
     println("Hello: " + meh(a))
 }
