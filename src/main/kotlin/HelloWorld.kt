9705dad78a822234872368790fa5f0af6237f40c
Tony McCrae <tony@eelpieconsulting.co.uk>
2018-11-09 11:51:58 +0000

    Vaguely sensible list factory method.

--- src/main/kotlin/HelloWorld.kt
+++ src/main/kotlin/HelloWorld.kt
@@ -1,18 +1,11 @@
-val a = 2
-
-fun meh() = if (a > 1) {
+fun meh(a: Int) = if (a > 1) {
     "ok"
 } else {
     "meh"
 }
 
-
 fun main(args: Array<String>) {
-    var mutable = a
-    mutable = mutable + 1
-    mutable += 1
-
-    val teamplate = "Hello $a"
-
-    println("Hello: " + meh() + mutable)
+    val things = listOf("a", "b", "c")
+    val a = things.size
+    println("Hello: " + meh(a))
 }
